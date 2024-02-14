dac5c6ab9e7212074c7fd4566f9827a8d5d912c3
akatsay <akatsay2@gmail.com>
2024-02-14 18:47:23 -0500

    setup https server

--- server/dist/main.js
+++ server/dist/main.js
@@ -2,12 +2,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors({
-        methods: 'GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS',
-    });
-    await app.listen(5000);
+    try {
+        const privateKeyPath = process.env.PRIVATE_KEY_PATH || path.join(__dirname, '../privkey.pem');
+        const certificatePath = process.env.CERTIFICATE_PATH || path.join(__dirname, '../fullchain.pem');
+        const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+        const certificate = fs.readFileSync(certificatePath, 'utf8');
+        const credentials = { key: privateKey, cert: certificate };
+        const app = await core_1.NestFactory.create(app_module_1.AppModule);
+        const httpsServer = https.createServer(credentials, app.getHttpAdapter().getInstance());
+        const port = 443;
+        app.enableCors({
+            methods: 'GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS',
+        });
+        await app.listen(5000);
+        httpsServer.listen(port, () => {
+            console.log(`Server running on https://localhost:${port}`);
+        });
+    }
+    catch (error) {
+        console.error('Error during startup:', error);
+    }
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as https from 'https';
+import * as path from 'path';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    // origin: '*',
-    methods: 'GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS',
-  });
-  await app.listen(5000);
+  try {
+    const privateKeyPath = process.env.PRIVATE_KEY_PATH || path.join(__dirname, '../privkey.pem');
+    const certificatePath = process.env.CERTIFICATE_PATH || path.join(__dirname, '../fullchain.pem');
+
+    const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+    const certificate = fs.readFileSync(certificatePath, 'utf8');
+    const credentials = { key: privateKey, cert: certificate };
+
+    const app = await NestFactory.create(AppModule);
+    const httpsServer = https.createServer(credentials, app.getHttpAdapter().getInstance());
+
+    const port = 443;
+    
+    app.enableCors({
+      // origin: '*',
+      methods: 'GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS',
+    });
+    await app.listen(5000);
+    httpsServer.listen(port, () => {
+      console.log(`Server running on https://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error during startup:', error);
+  }
 }
+
 bootstrap();
